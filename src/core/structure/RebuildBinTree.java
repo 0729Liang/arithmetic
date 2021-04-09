016240ede6423612508fe09ccfdd63604fd27ca0
yuanmingliang <yuanmingliang@360.cn>
2021-04-09 18:15:00 +0800

    更新重建二叉树与顺序打印二叉树的算法

--- src/core/structure/RebuildBinTree.java
+++ src/core/structure/RebuildBinTree.java
@@ -1,6 +1,7 @@
 package core.structure;
 
 import core.been.TreeNode;
+import core.tree.BinTreeSeq;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,43 +15,57 @@ import java.util.Map;
 public class RebuildBinTree {
 
     public static void main(String[] args) {
-        int[] pres = new int[]{3,9,20,15,7};
-        int[] ins = new int[]{9,3,15,20,7};
+        int[] pres = new int[]{3, 9, 20, 15, 7};
+        int[] ins = new int[]{9, 3, 15, 20, 7};
 
         TreeNode root = new RebuildBinTree().buildTree(pres, ins);
         System.out.println(new BinTreeSeq().levelOrder(root));
         System.out.println(new BinTreeSeq().levelOrder2(root));
     }
-    Map<Integer, Integer> inMap = new HashMap<>();// 记录中序遍历的数据，k=数组中具体的值，v=数组中此值的索引
+
+    Map<Integer, Integer> midMap = new HashMap<>();// 记录中序遍历的数据，k=数组中具体的值，v=数组中此值的索引
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // 记录中序遍历的kv
+        // 0.记录中序遍历的kv
         for (int i = 0; i < inorder.length; i++) {
-            inMap.put(inorder[i], i);
+            midMap.put(inorder[i], i);
         }
 
         int length = preorder.length;
-        return build(preorder,inorder,0, length-1,0,length-1);
+        // 先序、先序遍历的右指针初始值是length - 1，如果是length则越界
+        return build(preorder, 0, length - 1, 0, length - 1);
     }
 
-    public TreeNode build(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRight) {
+    /**
+     * 描述：重建二叉树
+     * 前序遍历 preorder = [3,9,20,15,7]
+     * 中序遍历 inorder = [9,3,15,20,7]
+     *
+     * @param preorder 先序遍历的数组
+     * @param preLeft  先序遍历的左指针
+     * @param preRight 先序遍历的右指针
+     * @param midLeft  中序遍历的左指针
+     * @param midRight 中序遍历的右指针
+     */
+    public TreeNode build(int[] preorder, int preLeft, int preRight, int midLeft, int midRight) {
         if (preLeft > preRight) {
             return null;
         }
 
         // 1. 前序遍历的第一个位置就是根节点
-        int preRoot = preLeft;
-        int rootVal = preorder[preRoot];
+        int rootIndex = preLeft;
+        int rootVal = preorder[rootIndex];
         // 2. 找到中序遍历中根节点的位置
-        int inRoot = inMap.get(rootVal);
+        rootIndex = midMap.get(rootVal);
         // 3. 从中序遍历中计算出左子树中的节点数目
-        int leftTreeSize = inRoot - inLeft;
+        int leftTreeSize = rootIndex - midLeft;
+        // 先序：根左右, 按照先序遍历顺序构建二叉树
         // 4. 构建根节点
         TreeNode root = new TreeNode(rootVal);
         // 5. 递归左子树: 先序遍历中「从 左边界+1 开始的 leftTreeSize」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
-        root.left = build(preorder, inorder, preLeft + 1, preLeft+leftTreeSize, inLeft, inRoot - 1);
+        root.left = build(preorder, preLeft + 1, preLeft + leftTreeSize, midLeft, rootIndex - 1);
         // 6. 递归右子树: 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
-        root.right = build(preorder, inorder, preLeft + leftTreeSize + 1, preRight, inRoot + 1, inRight);
+        root.right = build(preorder, preLeft + 1 + leftTreeSize, preRight, rootIndex + 1, midRight);
         return root;
     }
 
--- src/core/tree/BinTreeSeq.java
+++ src/core/tree/BinTreeSeq.java
@@ -1,9 +1,10 @@
-package core.structure;
+package core.tree;
 
 import core.been.TreeNode;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 描述：给定一个二叉树，返回该二叉树层序遍历的结果，（从左到右，一层一层地遍历）
@@ -16,32 +17,29 @@ public class BinTreeSeq {
      * @return int整型ArrayList<ArrayList <>>
      */
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> resList = new ArrayList<>();
         if (root == null) {
-            return result;
+            return resList;
         }
-        LinkedList<TreeNode> stack = new LinkedList<>();
-        stack.add(root);
-
-        while (!stack.isEmpty()) {
-            int size = stack.size();
-            ArrayList<Integer> list = new ArrayList<>();
-            // 遍历一层
+        Queue<TreeNode> quque = new LinkedList<>();
+        quque.offer(root);
+        while (!quque.isEmpty()) {
+            int size = quque.size();
+            ArrayList<Integer> tmpList = new ArrayList<>();
             while (size > 0) {
-                TreeNode node = stack.pollFirst();
-                list.add(node.val);
-                // 新的一层入栈
+                TreeNode node = quque.poll();
+                tmpList.add(node.val);
                 if (node.left != null) {
-                    stack.add(node.left);
+                    quque.offer(node.left);
                 }
                 if (node.right != null) {
-                    stack.add(node.right);
+                    quque.offer(node.right);
                 }
                 size--;
             }
-            result.add(list);
+            resList.add(tmpList);
         }
-        return result;
+        return resList;
     }
 
     /**
@@ -55,37 +53,46 @@ public class BinTreeSeq {
         LinkedList<TreeNode> stack = new LinkedList<>();
         stack.add(root);
 
-        int length = getLength(root);
+        int length = getTreeNodeDeep(root);
         while (!stack.isEmpty() && length > 0) {
             int size = stack.size();
-            ArrayList<Integer> list = new ArrayList<>();
+            ArrayList<Integer> tmpList = new ArrayList<>();
             // 遍历一层
             while (size > 0) {
                 TreeNode node = stack.pollFirst();
-                if (node == null) {
-                    list.add(null);
-                } else {
-                    list.add(node.val);
-                }
+                tmpList.add(node == null ? null : node.val);
+                stack.add(node == null ? null : node.left);
+                stack.add(node == null ? null : node.right);
                 size--;
-                stack.add(node == null?null:node.left);
-                stack.add(node == null?null:node.right);
             }
-            result.add(list);
+            result.add(tmpList);
             length--;
         }
         return result;
     }
 
 
-    public int getLength(TreeNode root){
+    /**
+     * 描述：返回二叉树的深度
+     */
+    public int getTreeNodeDeep(TreeNode root) {
         // 如果当前节点为空，说明已经到达最底层
-        if (root == null){
+        if (root == null) {
             return 0;
         }
-        int left=getLength(root.left);
-        int right=getLength(root.right);
+        int left = getTreeNodeDeep(root.left);
+        int right = getTreeNodeDeep(root.right);
         // 返回左右节点的深度的最大值，再加上自身的一层就是当前节点的深度
-        return Math.max(left,right)+1;
+        return Math.max(left, right) + 1;
+    }
+
+    /**
+     * 描述：返回二叉树的节点数
+     */
+    private int getTreeNodeSize(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return 1+ getTreeNodeSize(root.left)+ getTreeNodeSize(root.right);
     }
 }
